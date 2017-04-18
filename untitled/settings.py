45c8d9d718785ce8e5d9e43525ef4c9cefd8b343
yam-b <yamb@mail.tau.ac.il>
2017-04-18 17:15:09 +0200

    fixed css (hopefully)

--- untitled/settings.py
+++ untitled/settings.py
@@ -113,4 +113,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost', '.handasaim.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost', '.handasaim.com', u'www.handasaim.com']
