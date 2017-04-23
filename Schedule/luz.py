a250dec8f1e00b4d8cff2658a2a2a70661759ec6
yam-b <yamb@mail.tau.ac.il>
2017-04-23 15:30:53 +0300

    faster heb weekday and month (use tuple)

--- Schedule/luz.py
+++ Schedule/luz.py
@@ -10,30 +10,48 @@ TEXT_TO_FIND = u'מערכת שעות'
 UP_CUT = 0
 LEFT_CUT = 1
 DEFAULT_INFO = u''
-
-
-def school_time(time):
-    return {
-        0: ['7:45', '8:30'],
-        1: ['8:30', '9:15'],
-        2: ['9:15', '10:00'],
-        3: ['10:15', '11:00'],
-        4: ['11:00', '11:45'],
-        5: ['12:10', '12:55'],
-        6: ['12:55', '13:40'],
-        7: ['13:50', '14:35'],
-        8: ['14:35', '15:20'],
-        9: ['15:25', '16:10'],
-        10: ['16:10', '16:55'],
-        11: ['16:55', '17:40'],
-        12: ['17:40', '18:25']
-    }[time]
+HEB_WEEKDAY = (
+    u'ראשון',
+    u'שני',
+    u'שלישי',
+    u'רביעי',
+    u'חמישי',
+    u'שישי',
+    u'שבת')
+HEB_MONTH = (
+    u'ינואר',
+    u'פברואר',
+    u'מרץ',
+    u'אפריל',
+    u'מאי',
+    u'יוני',
+    u'יולי',
+    u'אוגוסט',
+    u'ספטמבר',
+    u'אוקטובר',
+    u'נובמבר',
+    u'דצמבר')
 
 
 def row(sheet, index, left_cut):
     row = sheet.iloc[index, left_cut:]
     lst = []
     is_empty = 1
+    school_time = (
+        ('7:45', '8:30'),
+        ('8:30', '9:15'),
+        ('9:15', '10:00'),
+        ('10:15', '11:00'),
+        ('11:00', '11:45'),
+        ('12:10', '12:55'),
+        ('12:55', '13:40'),
+        ('13:50', '14:35'),
+        ('14:35', '15:20'),
+        ('15:25', '16:10'),
+        ('16:10', '16:55'),
+        ('16:55', '17:40'),
+        ('17:40', '18:25')
+    )
     for i in row:
         if pd.isnull(i):
             i = ''
@@ -43,7 +61,7 @@ def row(sheet, index, left_cut):
     html_entities = '<small style="color:#a1a4a5">{}</small><br><b>{}</b><br><small style="color:#a1a4a5">{}</small>'
     if index:
         index -= 1
-        lst = [html_entities.format(school_time(index)[0], index, school_time(index)[1])] + lst
+        lst = [html_entities.format(school_time[index][0], index, school_time[index][1])] + lst
     if is_empty: return []
     return lst
 
@@ -56,18 +74,6 @@ def b(url, text):
 def to_table(url, up_cut, left_cut):
     sheet = pd.ExcelFile(url).parse(0)
     return [row(sheet, i, left_cut) for i in range(up_cut, len(sheet.index))]
-
-
-def to_heb_day(day):
-    return {
-        '0': u'ראשון',
-        '1': u'שני',
-        '2': u'שלישי',
-        '3': u'רביעי',
-        '4': u'חמישי',
-        '5': u'שישי',
-        '6': u'שבת'
-    }[day]
 
 
 def news_list(url):
@@ -100,23 +106,6 @@ def news_list(url):
     return list
 
 
-def to_heb_month(month):
-    return {
-        '01': u'ינואר',
-        '02': u'פברואר',
-        '03': u'מרץ',
-        '04': u'אפריל',
-        '05': u'מאי',
-        '06': u'יוני',
-        '07': u'יולי',
-        '08': u'אוגוסט',
-        '09': u'ספטמבר',
-        '10': u'אוקטובר',
-        '11': u'נובמבר',
-        '12': u'דצמבר'
-    }[month]
-
-
 def schedule_info(url=URL, text_to_find=TEXT_TO_FIND, default_info=DEFAULT_INFO, up_cut=UP_CUT, left_cut=LEFT_CUT):
     error = 0
     try:
@@ -127,7 +116,7 @@ def schedule_info(url=URL, text_to_find=TEXT_TO_FIND, default_info=DEFAULT_INFO,
         link = headline.find_next_sibling('a')['href'].strip()
         table = to_table(link, up_cut, left_cut)  # ONLINE: to_table(link) OFFLINE: to_table(local)
         date = headline.find_previous_sibling('sup').text[1:-1]
-        date = date[:2] + u' ב' + to_heb_month(date[3:5])
+        date = date[:2] + u' ב' + HEB_MONTH[int(date[3:5])]
     except:
         title = 'אין מערכת'
         day = dt.date.today().strftime('%d')
@@ -145,9 +134,8 @@ def schedule_info(url=URL, text_to_find=TEXT_TO_FIND, default_info=DEFAULT_INFO,
         list = news_list(url)
     except:
         list = []
-    weekday = dt.datetime.strptime(day + month + str(dt.datetime.now().year), '%d%m%Y').date().strftime(
-        '%w')
-    time = u'יום ' + to_heb_day(weekday) + ', ' + day + u' ב' + to_heb_month(month)
+    weekday = dt.datetime.strptime(day + month + str(dt.datetime.now().year), '%d%m%Y').date().strftime('%w')
+    time = u'יום ' + HEB_WEEKDAY[int(weekday)] + ', ' + day + u' ב' + HEB_MONTH[int(month) - 1]
 
     return {'headline_text': text_to_find, 'headline_link': link, 'url': url, 'title': title, 'info': description,
             'link': link,
